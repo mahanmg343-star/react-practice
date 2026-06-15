9523f96d25872f9608877396f4955b0044338a19
mahanmg343-star <mahanmg343@gmail.com>
2026-06-15 23:11:56 +0330

    Update Sec2.jsx

--- practice1/src/Sec2.jsx
+++ practice1/src/Sec2.jsx
@@ -1,8 +1,83 @@
+/* eslint-disable react/style-prop-object */
 import React from 'react'
 
 function sec2() {
   return (
-   <h6>u</h6>
+   <>
+   <div className=' align-items-center g-5 mb-5 p-3'>
+   <h2>برنامه نویسی با راحت یاد</h2>
+    <h5>انجام پروژه های بین المللی با بهترین کیفیت</h5>
+    <h6>برگزاری انواع دوره ها با بهترین اساتید</h6>
+   </div>
+
+<div className='w-100 d-flex justify-content-center align-items-center'>
+
+   <div className="card width: 13rem; ml-2">
+  <img src="https://www.rahatyad.ir/images/Figma.png"  className="card-img-top w-auto d-flex justify-content-center align-items-center " alt="" />
+  <div className="card-body">
+    <h5 className="card-title mt-3">دوره جامع آموزش فیگما</h5>
+    <h5 className="card-text p-4 d-flex ju text-primary">مشاهده</h5>
+    <a href="https://www.rahatyad.ir/Figma.php" className="btn btn-primary w-50">خرید</a>
+  </div>
+</div>
+<div className="card width: 13rem; w-5 m-1">
+  <img src="https://www.rahatyad.ir/images/Figma.png" className="card-img-top w-auto d-flex justify-content-center align-items-center" alt="" />
+  <div className="card-body">
+    <h5 className="card-title mt-3">دوره جامع آموزش فیگما</h5>
+    <h5 className="card-text p-4 d-flex ju text-primary">مشاهده</h5>
+    <a href="https://www.rahatyad.ir/Figma.php" className="btn btn-primary w-50">خرید</a>
+  </div>
+</div>
+<div className="card width: 13rem; w-5 m-1">
+  <img src="https://www.rahatyad.ir/images/Figma.png" className="card-img-top w-auto d-flex justify-content-center align-items-center" alt="" />
+  <div className="card-body">
+    <h5 className="card-title mt-3">دوره جامع آموزش فیگما</h5>
+    <h5 className="card-text p-4 d-flex ju text-primary">مشاهده</h5>
+    <a href="https://www.rahatyad.ir/Figma.php" className="btn btn-primary w-50">خرید</a>
+  </div>
+</div>
+<div className="card width: 13rem; w-5 mr-2">
+  <img src="https://www.rahatyad.ir/images/Figma.png" className="card-img-top w-auto d-flex justify-content-center align-items-center" alt="" />
+  <div className="card-body">
+    <h5 className="card-title mt-3">دوره جامع آموزش فیگما</h5>
+    <h5 className="card-text p-4 d-flex ju text-primary">مشاهده</h5>
+    <a href="https://www.rahatyad.ir/Figma.php" className="btn btn-primary w-50">خرید</a>
+  </div>
+</div>
+</div>
+ <div className='w-100 d-flex justify-content-center align-items-center'>
+
+   <div className="card width: 13rem; m-4">
+  <img src="https://www.rahatyad.ir/images/Figma.png"  className="card-img-top w-auto d-flex justify-content-center align-items-center " alt="" />
+  <div className="card-body">
+    <h5 className="card-title mt-3">دوره جامع آموزش فیگما</h5>
+    <h5 className="card-text p-4 d-flex ju text-primary">مشاهده</h5>
+    <a href="https://www.rahatyad.ir/Figma.php" className="btn btn-primary w-50">خرید</a>
+  </div>
+</div>
+<div className="card width: 13rem; w-5 ">
+  <img src="https://www.rahatyad.ir/images/Figma.png" className="card-img-top w-auto d-flex justify-content-center align-items-center" alt="" />
+  <div className="card-body">
+    <h5 className="card-title mt-3">دوره جامع آموزش فیگما</h5>
+    <h5 className="card-text p-4 d-flex ju text-primary">مشاهده</h5>
+    <a href="https://www.rahatyad.ir/Figma.php" className="btn btn-primary w-50">خرید</a>
+  </div>
+</div>
+<div className="card width: 13rem; w-5 m-4">
+  <img src="https://www.rahatyad.ir/images/Figma.png" className="card-img-top w-auto d-flex justify-content-center align-items-center" alt="" />
+  <div className="card-body">
+    <h5 className="card-title mt-3">دوره جامع آموزش فیگما</h5>
+    <h5 className="card-text p-4 d-flex ju text-primary">مشاهده</h5>
+    <a href="https://www.rahatyad.ir/Figma.php" className="btn btn-primary w-50">خرید</a>
+  </div>
+</div>
+
+
+</div>
+
+
+
+   </>
   )
 }
 
